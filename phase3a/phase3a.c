2918df1249a5363a1dc7b0c74fd3281624f5aa92
Raymond Peter Rea <raymondprea@cs.arizona.edu>
2021-04-15 01:54:37 -0700

    need to finish handler/pager

--- phase3a/phase3a.c
+++ phase3a/phase3a.c
@@ -8,30 +8,104 @@
 
 #include <assert.h>
 #include <phase1.h>
-#include <phase2.h>
+#include <phase2.>
 #include <usloss.h>
 #include <string.h>
 #include <libuser.h>
 
 #include "phase3Int.h"
 
-P3_VmStats  P3_vmStats;
+#define CHECKKERNEL() \
+    if ((USLOSS_PsrGet() & USLOSS_PSR_CURRENT_MODE) == 0) USLOSS_IllegalInstruction()
+
+#define LOCK(lid) { \
+    int _rc = P1_Lock(lid); \
+    assert(_rc == P1_SUCCESS); \
+}
+
+#define UNLOCK(lid) { \
+    int _rc = P1_Unlock(lid); \
+    assert(_rc == P1_SUCCESS); \
+}
+
+#define WAIT(vid) { \
+    int _rc = P1_Wait(vid); \
+    assert(_rc == P1_SUCCESS); \
+}
+
+//P3_VmStats P3_VmStats;
+
+P3_VmStats *stats;
 
 // TODO: use this? or can faults just be tracked with an id, have an int array
-typedef struct Fault {
+// TODO what other fields should go here?
+
+typedef struct Fault Fault;
+
+struct Fault {
     int pid;    // process that's faulting
     int page;   // page on which process is faulting
     int frame;  // not sure if this remains unassigned initially, P3PageFaultResolve seems to assign this later
-} Fault;
+    Fault *next;
+};
 
 // queue of faults
 // locks to protect accessing queue of faults b/c shared b/t FaultHandler and Pager
 
+//locks
+int PAGER_LOCK;
+int FALUTQ_LOCK;
+int STATS_LOCK;
+
+//condition variables 
+int Q_NOT_EMPTY;
+//TODO should this go in the fault struct?
+int FAULT_HANDLED;
+int PAGER_QUIT;
+
+//pager will use this as state variable 
+Fault *HEAD;
+//fault handler will use this as state variable 
+int PAGER_DONE = FALSE;
+//shutdown will use this as a state variable
+int pagerQuit = FALSE;
+
+int STARTUP  = FALSE;
 int SHUTDOWN = FALSE;
+
+//TODO do i need an array of PTEs?
+USLOSS_PTE tables[P1_MAXPROC];
+
+static void InitStub(USLOSS_Sysargs *sysargs);
+static void ShutdownStub(USLOSS_Sysargs *sysargs);
+
+int enQFault(Fault *fault) {
+    if (HEAD == NULL) {
+        HEAD = fault;
+        return;
+    }
+    
+    Fault *curr = HEAD;
+    while (curr->next != NULL) {
+        curr = curr->next;
+    }
+    curr->next = fault;
+    return 0;
+}
+
+int deQFault() {
+    if (HEAD == NULL) {return;}
+
+    Fault *removed = HEAD;
+    HEAD = HEAD->next;
+    free(removed);
+    return 0;
+}
 
 static void
 FaultHandler(int type, void *arg)
 {
+    int rc;
     /*******************
 
     if it's an access fault (USLOSS_MmuGetCause)
@@ -45,7 +119,36 @@ FaultHandler(int type, void *arg)
     *********************/
     int cause = USLOSS_MmuGetCause();
     if (cause == USLOSS_MMU_ACCESS) { // if it's an access fault
-        // terminate process with P3_ACCESS_VIOLATION;
+        P2_Terminate(P3_ACCESS_VIOLATION);
+    } else {
+        //TODO how do i get the rest of the info? 
+        Fault *newFault = malloc(sizeof(Fault));
+        newFault->pid  = P1_GetPid();
+        newFault->next = NULL;
+
+        // add fault to queue
+        LOCK(FAULTQ_LOCK);
+        rc = enQFault(newFault);
+        assert(rc == 0);
+        
+        // signal pager 
+        rc = P1_Signal(Q_NOT_EMPTY);
+        assert(rc == P1_SUCCESS);
+        UNLOCK(FAULTQ_LOCK);
+
+        // wait until fault has been handled 
+        LOCK(PAGER_LOCK);
+        while(!PAGER_DONE) {
+            WAIT(FAULT_HANDLED);
+        }
+        PAGER_DONE = FALSE;
+        UNLOCK(PAGER_LOCK);
+
+        // terminate the process if necessary
+        // TODO how do i know if it's necessary?
+        // rc = P2_Terminate(P3_OUT_OF_SWAP);
+        // assert(rc == P1_SUCCESS);
+        
     }
 
 }
@@ -84,12 +187,47 @@ int
 P3_VmInit(int unused, int pages, int frames, int pagers)
 {
     CheckMode();
+    int rc;
+    int pagerPid;
 
-    // zero P3_vmStats
-    // initialize fault queue, lock, and condition variable
-    // call P3FrameInit
-    // call P3SwapInit
-    // fork pager
+    if (startup == TRUE) {return P3_ALREADY_INITIALIZED;}
+    if (pages < 0)  {return P3_INVALID_NUM_PAGES;}
+    if (frams < 0)  {return P3_INVALID_NUM_FRAMES;}
+    if (pagers < 0) {return P3_INVALID_NUM_PAGERS;}
+
+    // zero stats
+    stats = malloc(sizeof(P3_VmInit));
+    stats->pages      = 0;
+    stats->frames     = 0;
+    stats->blocks     = 0;
+    stats->freeFrames = 0;
+    stats->freeBlocks = 0;
+    stats->faults     = 0;
+    stats->newPages   = 0;
+    stats->pageIns    = 0;
+    stats->pageOuts   = 0;
+    stats->replaced   = 0;    
+
+    // initilizing fault queue, lock and cond var
+    HEAD = NULL; 
+    rc = P1_LockCreate("fault q lock", &FAULTQ_LOCK);
+    assert(rc == P1_SUCCESS);
+    rc = P1_CondCreate("q not empty", FAULTQ_LOCK, &Q_NOT_EMPTY);
+
+    // call P3FrameInit, P3SwapInit
+    rc = P3FrameInit(pages, frames);
+    assert(rc == P1_SUCCESS);
+    rc = P3SwapInit(pages, frames);
+    assert(rc == P1_SUCCESS);
+
+    //numMaps is not used, passing in 0, to avoid null arg err
+    rc = USLOSS_MmuInit(0, pages, frames);
+    assert(rc == USLOSS_MMU_OK);
+ 
+    STARTUP = TRUE;
+
+    // fork pager                
+    rc = P1_Fork("pager", Pager, P3_MAX_PAGERS, USLOSS_MIN_STACK, &pagerPid);
 
     USLOSS_IntVec[USLOSS_MMU_INT] = FaultHandler;
     return P1_SUCCESS;
@@ -99,8 +237,20 @@ void
 P3_VmShutdown(void)
 {
     CheckMode();
-    
-    // cause pager to quit
+    int rc;
+    // do nothing if P3_VmInit hasn't been called
+    if (!startup) {return;}
+
+    // waiting for the pager to finish
+    LOCK(PAGER_LOCK); 
+    while(!pagerQuit) {
+        WAIT(PAGER_QUIT);
+    }    
+    UNLOCK(PAGER_LOCK);
+
+    rc = USLOSS_MmuDone();
+    assert(rc == USLOSS_MMU_OK);    
+
     P3_PrintStats(&P3_vmStats);
 
     SHUTDOWN = TRUE;
@@ -111,8 +261,16 @@ P3_AllocatePageTable(int pid)
 {
     CheckMode();
 
+    // if P3_VmInit hasn't been called 
+    if (!STARTUP) {return NULL;}
+
     USLOSS_PTE  *table = NULL;
-    // create a new page table here
+    table = malloc(sizeof(USLOSS_PTE));
+   
+    // setting the newly created PTE into the array 
+    // of proccesses PTEs
+    tables[pid] = table;  
+ 
     return table;
 }
 
@@ -120,16 +278,29 @@ void
 P3_FreePageTable(int pid)
 {
     CheckMode();
+    int rc;
+    // do nothing if init hasn't been called
+    if (!STARTUP) {return;}
 
-    // free the page table here
+    rc = P3FrameFreeAll(pid);
+    assert(rc == P1_SUCCESS);
+  
+    rc = P3SwapFreeAll(pid);
+    assert(rc == P1_SUCCESS);
+
+    // freeing this processes page table 
+    USLOSS_PTE *freed = tables[pid]
+    free(freed);
+    tables[pid] = NULL;
 }
 
 int
 P3PageTableGet(PID pid, USLOSS_PTE **table)
 {
     CheckMode();
-    
-    *table = NULL;
+    if (pid < 0 || pid >= P1_MAXPROC) {return P1_INVALID_PID;}
+
+    *table = tables[pid];
     return P1_SUCCESS;
 }
 
@@ -141,15 +312,48 @@ int P3_Startup(void *arg)
     int pid4;
     int status;
     int rc;
+    int i;
 
     rc = Sys_Spawn("P4_Startup", P4_Startup, NULL,  3 * USLOSS_MIN_STACK, 2, &pid4);
     assert(rc == 0);
     assert(pid4 >= 0);
+
+    // initializing array of page tables
+    for (i=0; i < P1_MAXPROC; i++){
+        tables[i] = NULL;
+    }    
+
+    rc = P1_LockCreate("pager lock", &PAGER_LOCK);
+    assert(rc == P1_SUCCESS);
+    rc = P1_LockCreate("fault lock", &FAULT_LOCK);
+    assert(rc == P1_SUCESS);
+    rc = P1_CondCreate("fault handled", PAGER_LOCK, &FAULT_HANDLED);
+    assert(rc == P1_SUCCESS);
+    rc = P1_CondCreate("pager quit", PAGER_LOCK, &PAGER_QUIT);
+    assert(rc == P1_SUCCESS);
+
     rc = Sys_Wait(&pid, &status);
     assert(rc == 0);
     assert(pid == pid4);
     Sys_VmShutdown();
     return 0;
+}
+
+static void InitStub(USLOSS_Sysargs *sysargs) {
+    int rc;
+
+    sysargs->arg4 = P3_VmInit(0, sysargs->arg2, sysargs->arg3, sysargs->arg4);
+
+    void **junk0;
+    int  *junk1;
+    int  *junk2;
+    int  *junk3;
+    rc = USLOSS_MmuGetConfig(&sysargs->arg1, junk0, &((int*) sysargs->arg2), junk1, junk2, junk3);
+    assert(rc == USLOSS_MMU_OK);    
+}
+
+static void ShutdownStub(USLOSS_Sysargs *sysargs) {
+    P3_VmShutdown();
 }
 
 void
