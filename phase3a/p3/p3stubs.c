bdf28c9c28fbd4c10957387d69069cba15a56368
Annie G <anniegao@cs.arizona.edu>
2021-04-16 09:00:03 -0700

    usloss changes from hartman

--- phase3a/p3/p3stubs.c
+++ phase3a/p3/p3stubs.c
@@ -15,16 +15,11 @@
 // Phase 3c
 
 int P3FrameInit(int pages, int frames) {return P1_SUCCESS;}
-int P3FrameShutdown(void) {return P1_SUCCESS;}
-int P3FrameAllocate(PID pid, int *frame) {return P1_SUCCESS;}
 int P3FrameFreeAll(PID pid) {return P1_SUCCESS;}
-int P3PagerInit(int pages, int frames, int pagers) {return P1_SUCCESS;}
-int P3PagerShutdown(void) {return P1_SUCCESS;}
 
 // Phase 3d
 
 int P3SwapInit(int pages, int frames) {return P1_SUCCESS;}
-int P3SwapShutdown(void) {return P1_SUCCESS;}
 int P3SwapFreeAll(PID pid) {return P1_SUCCESS;}
-int P3SwapClock(PID pid, int *frame) {return P1_SUCCESS;}
+int P3SwapOut(int *frame) {return P1_SUCCESS;}
 int P3SwapIn(PID pid, int page, int frame) {return P1_SUCCESS;}
